12c9f0634321148e29de59df8c23a908254b9f46
Pablo Ramirez Zavala <a00844903@tec.mx>
2026-06-09 16:47:34 -0600

    Schedule_Engine header
    I just added the schedule_engine header, which is just the declaration of variables/methods to the class.

---
+++ include/schedule_engine.hpp
@@ -0,0 +1,28 @@
+#ifndef SCHEDULE_ENGINE_H
+#define SCHEDULE_ENGINE_H
+class instructor;
+class course;
+class space;
+
+class schedule_engine {
+private:
+    instructor* instructors_array;
+    course* courses_array;
+    space* spaces_array;
+    
+    int num_instructors;
+    int num_courses;
+    int num_spaces;
+
+public:
+    // Constructor y Destructor
+    schedule_engine();
+    ~schedule_engine();
+
+    // Métodos de operación
+    bool room_assignment(course* cour, space* spac);
+    bool lecturer_limit_classes();
+    bool exit_program();
+};
+
+#endif
