952fdb5feb9a841d0d4087b843a0073fe499e15b
saerangato <saerangatowo@gmail.com>
2026-06-10 12:21:39 -0600

    cpp de course y schedule

---
+++ src/course.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include"course.hpp"
+
+course::course(std::string courseCode, std::string courseName, std::string dep, int n_students) {
+    course_code = courseCode;
+    course_name = courseName;
+    department = dep;
+    num_students = n_students;
+}
+
+course::~course() {
+
+}
+
+void course::set_instructor(instructor* inst) {
+    assigned_instructor.instructor(inst);
+}
+
+instructor* instructor::get_instructor() {
+    return assigned_instructor;
+}
+
+void instructor::set_space(space* spac) {
+    assigned_space.space(spac);
+}
+
+space* course::get_space() {
+    return assigned_space;
+}
+
+void virtual course::print_details() {
+    std::cout<<"Course name: ", course_name, "\nCourse code: ", course_code, "\nDepartment: ", department, "\nNumber of students: ", num_students, "\nInstructor: ", assigned_instructor, "\nSpace: ", assigned_space, "\n";
+}
