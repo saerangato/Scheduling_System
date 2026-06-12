8b7895510aa1bcb57718cf2538b7ee62c771758e
Pablo Ramirez <a00844903@tec.mx>
2026-06-11 23:55:05 -0600

    subir

--- src/course.cpp
+++ src/course.cpp
@@ -1,28 +1,27 @@
-#include<iostream>
-#include<optional>
-#include<string>
-#include"course.hpp"
+#include <iostream>
+#include <optional>
+#include <string>
+#include "../include/course.hpp"
 
-course::course(std::string courseCode, std::string courseName, std::string dep, int n_students) {
+// Ajustamos el constructor para recibir los nuevos parámetros
+course::course(std::string courseCode, std::string courseName, std::string dep, int n_students, std::string day, int start, int end) {
     course_code = courseCode;
     course_name = courseName;
     department = dep;
     num_students = n_students;
+    course_day = day;
+    start_hour = start;
+    end_hour = end;
 }
 
-course::~course() {
-
-}
+course::~course() {}
 
 void course::set_instructor(instructor* inst) {
     assigned_instructor = inst;
 }
 
 instructor* course::get_instructor() {
-    if (assigned_instructor.has_value()) {
-        return assigned_instructor.value();
-    }
-    return nullptr;
+    return assigned_instructor.value_or(nullptr);
 }
 
 void course::set_space(space* spac) {
@@ -30,25 +29,12 @@ void course::set_space(space* spac) {
 }
 
 space* course::get_space() {
-    if (assigned_space.has_value()) {
-        return assigned_space.value();
-    }
-    return nullptr;
+    return assigned_space.value_or(nullptr);
 }
 
+// Estas son las funciones que el compilador te estaba pidiendo
+// para que el motor de horarios funcione correctamente:
+
 void course::print_details() {
-    std::cout<<"Course name: "<< course_name
-    << "\nCourse code: "<< course_code<<
-    "\nDepartment: "<< department
-    << "\nNumber of students: "<< num_students<< 
-    "\nInstructor: "; 
-    if (assigned_instructor.has_value())
-        std::cout << "\nInstructor assigned";
-    else
-        std::cout << "\nNo instructor assigned";
-    std::cout<<"\nSpace: "; 
-    if (assigned_instructor.has_value())
-        std::cout << "\nInstructor assigned"<<"\n";
-    else
-        std::cout << "\nNo instructor assigned"<< "\n";
+    std::cout << "Course: " << course_name << " (" << course_code << ")" << std::endl;
 }
